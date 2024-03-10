e0ba6b0924cdfa8b33edb5936126f9eeb6ba1d0f
Dmitriy <rassoldima@yandex.ru>
2024-03-10 06:30:22 +0300

    #	deleted:    routes/index.js

--- database/redis.js
+++ database/redis.js
@@ -1,8 +1,18 @@
 const { createClient } = require("redis");
 
+const localId =
+  "#" +
+  (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6) +
+  (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6) +
+  (Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6);
+
 const client = createClient();
+const pubClient = client.duplicate();
+const subClient = client.duplicate();
 
 client.connect();
+pubClient.connect();
+subClient.connect();
 
 async function get(key, defaultValue) {
   const value = await client.get(key);
@@ -18,11 +28,25 @@ async function set(key, value) {
 }
 
 async function setObject(key, value) {
-     await client.hSet(key, value);
- }
+  await client.hSet(key, value);
+}
 
- async function getObject(key) {
-   await client.hGet(key);
+async function getObject(key) {
+  await client.hGet(key);
+}
+
+async function pub(type, payload) {
+  payload.publisher = localId;
+  console.log(localId);
+  return pubClient.publish(type, JSON.stringify(payload));
+}
+
+async function sub(type, callback) {
+  subClient.subscribe(type, (message) => {
+    const payload = JSON.stringify(message);
+    if (payload.publisher === localId) return;
+    callback(payload);
+  });
 }
 
 module.exports = {
@@ -31,4 +55,6 @@ module.exports = {
   setObject,
   getObject,
   del: client.del,
+  pub,
+  sub,
 };
--- wsioapp.js
+++ wsioapp.js
@@ -1,17 +1,22 @@
+const messages = require("./database/inmem");
+const { pub, sub } = require("./database/redis");
 const { Server } = require("socket.io");
 
 module.exports = function (httpServer) {
   const wsServer = new Server(httpServer, {
-    cors: {
-      origin: "http://127.0.0.1:5500",
-      methods: ["GET", "POST"],
-    },
+    cors: "*",
   });
-
-  let messages = {};
-  //room id => [message]
-
+  sub("message", (payload) => {
+    // 'userId':  ..., 'to':to/user,  'toId': ..., 'message':'...'
+    messages[payload.to] = messages[payload.to] || [];
+    messages[payload.to].push(payload);
+    wsServer.to(payload.to).emit("message", payload);
+  });
   wsServer.on("connection", (socket) => {
+    //room id => [message]
+
+    const userId = socket.id;
+
     console.log("a user connected");
     socket.on("join", (msg) => {
       socket.join(msg.id);
@@ -20,13 +25,12 @@ module.exports = function (httpServer) {
       socket.leave(msg.id);
     });
     socket.on("send", (msg, reply) => {
-      messages[msg.room] = messages[msg.room] || [];
-      messages[msg.room].push(msg);
+      messages[msg.to] = messages[msg.to] || [];
+      messages[msg.to].push(msg);
+      msg.userId = userId;
+      socket.to(msg.to).emit("message", msg);
+      pub("message", msg);
       reply("ok");
-      socket.to(msg.room).emit("message", msg);
-    });
-    socket.on("get", (msg, reply) => {
-      reply(messages[msg.room] || []);
     });
   });
 };
